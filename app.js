81359b688612a4f6adaf5457b3eece2e9f780433
Arpon <a4arpon@gmail.com>
2022-12-27 18:53:03 +0600

    Live Upload --+-- Git 1 Time -> 7:00 Date -> 27:12:2022 Author -> Arpon  Changes to be committed: 	new file:   app.js 	new file:   arpon-x1-min.png 	new file:   blob.svg 	new file:   index.html 	new file:   style.css

---
+++ app.js
@@ -0,0 +1,8 @@
+// import Typed from "typed.js";
+var typed = new Typed('#post', {
+    strings: ["Full Stack Web Developer", "MERN Stack Developer", "Python Developer", "Server Side Engineer"],
+    typeSpeed: 80,
+    loop: true,
+    loopCount: Infinity,
+});
+  
